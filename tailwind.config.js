7b9cf3e8f4ac9803e207563ed538beb7d7b9f52f
Sabbir Hossain Rizvi <rizvibd1100@gmail.com>
2025-09-22 18:22:35 +0600

    Landing page - first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4227DC",
+        secondary: "#806EE8",
+        darkViolet: "#0E0830",
+        red: "hsl(0, 87%, 67%)",
+        grayishViolet: "hsl(257, 7%, 63%)",
+        veryDarkBlue: "hsl(255, 11%, 22%)",
+        veryDarkViolet: "hsl(260, 8%, 14%) ",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      spacing: {
+        180: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
